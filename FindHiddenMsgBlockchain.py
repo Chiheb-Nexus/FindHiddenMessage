94d391c25cdc9468526aa4d5dade912fb7b37028
Chiheb NeXus <chihebnexus@gmail.com>
2016-12-12 12:15:30 +0100

    Add regex choice

--- FindHiddenMsgBlockchain.py
+++ FindHiddenMsgBlockchain.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 #
 # Retrive Bitcoin raw blocks and decode it in order to find hidden messages
@@ -9,6 +9,7 @@ from json import loads
 from urllib.request import Request, urlopen
 from sys import exit
 import argparse
+import re
 
 class FindHiddenMsgBlockchain:
 	def __init__(self):	
@@ -18,24 +19,30 @@ class FindHiddenMsgBlockchain:
 		""")
 		parser.add_argument('-s', '--start', help = "First index of Bitcoin's block range", required = True, default = "1")
 		parser.add_argument('-f', '--final', help = "Last index of Bitcoin's block range", required = True, default = "2")
+		parser.add_argument('-r', '--regex', help = "Catch only human readable characters", required = True, default = "n")
 		number = parser.parse_args()
 		
 		try:
-			self.run(url = url_raw, start = number.start , stop = number.final)
+			self.run(url = url_raw, start = number.start , stop = number.final, regex = number.regex)
 			
 		except ValueError:
 			print("Error has occurred")
 			print("Please enter a valid integers for Start and Final arguments.")
 			exit(1)
 		
-	def run(self, url = "", start = "", stop = ""):
+	def run(self, url = "", start = "", stop = "", regex = ""):
+		
 		try:
 			for number in range(int(start), int(stop)+1):
 				data = self.get_block_hash(url = url, block_number = str(number))
 				
-				print("[+] Fetching data for block: \tNmber: {0}\tHash: {1}".format(number, data["data"]["hash"]))
-				self.run_output(block_hash = data["data"]["hash"], file_name = str(number))
+				print("[+] Fetching data for block: \tNumber: {0}\tHash: {1}".format(number, data["data"]["hash"]))
+				self.run_output(block_hash = data["data"]["hash"], file_name = str(number), reg = regex)
 				print("-> Data fetched.")
+				
+		except (TypeError, NameError):
+			print("Error occurred during fetching data")
+			exit(1)
 				
 		except KeyboardInterrupt:
 			user_data = input("\rDo you want to exit the program ? [Y/N]: ") 
@@ -47,18 +54,22 @@ class FindHiddenMsgBlockchain:
 	def get_block_hash (self, url = "", block_number = ""):
 		return self.get_raw_block(url = url, block_hash = block_number)
 		
-	def run_output(self, block_hash = "", file_name = ""):
+	def run_output(self, block_hash = "", file_name = "", reg = ""):
 		url_explorer = "https://blockexplorer.com/api/rawblock/"
-		n = 70 # Split the decoded output every 70 characters for better visualisation of the output file
+		n = 100 # Split the decoded output every 70 characters for better visualisation of the output file
 		
 		data = self.get_raw_block(url = url_explorer, block_hash = block_hash)
 		ascii_data = self.return_ascii_from_hex(data["rawblock"])
 		ascii_splited = [ascii_data[i:i+n] for i in range(0, len(ascii_data), n)]
+		regex = re.compile(r"[a-zA-Z0-9:-@.]+")
 		
 		try:
-			with open(file_name, 'a') as output: # msg_raw_block is the name of the output file
-				for splited in ascii_splited:		   # change it whaterver you want or keep it :-)	
-					output.write(splited + "\n")
+			with open(file_name, 'a') as output: 
+				for splited in ascii_splited:
+					if reg == 'y':
+						output.write("".join(result for result in regex.findall(splited)) + "\n")
+					if reg == 'n':
+						output.write(splited + "\n")
 				
 		except Exception as e:
 			print("Error occurred wile opening/writing into the file\n", e)
